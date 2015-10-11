3194abc942fad40afdf4d6c74a37178b5f9faed8
ngonzales <nathan_gonzales94@yahoo.com>
2015-10-11 15:16:05 -0600

    Analysis file added

---
+++ src/main/java/vcreature/mainSimulation/Analysis.java
@@ -0,0 +1,20 @@
+package vcreature.mainSimulation;
+
+import vcreature.phenotype.Creature;
+
+/**
+ * Created by Dayloki on 10/11/2015.
+ */
+public class Analysis
+{
+
+  public Analysis()
+  {
+  
+  }
+  public void CheckValidlyOfCreature(Creature creature)
+  {
+
+  }
+
+}
