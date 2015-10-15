aed6b189672909034d4621fe3e0fa2abf369cfe6
James Holland <jamesfholland@gmail.com>
2015-10-15 16:42:50 -0600

    Added mutator placeholders for mutation types.

---
+++ src/main/java/vcreature/mutator/Subtracter.java
@@ -0,0 +1,8 @@
+package vcreature.mutator;
+
+/**
+ * Removes genes from a genome, either random or seeded.
+ */
+public class Subtracter
+{
+}
---
+++ src/main/java/vcreature/mutator/Symmetrizer.java
@@ -0,0 +1,8 @@
+package vcreature.mutator;
+
+/**
+ * Symmetrizer (real word) makes a creature or block symmetrical.
+ */
+public class Symmetrizer
+{
+}
