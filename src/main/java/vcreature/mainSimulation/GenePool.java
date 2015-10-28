5e284574cf7388dc6c78e91207c95a286193ccb7
James Holland <jamesfholland@gmail.com>
2015-10-28 01:18:54 -0600

    Changed the code to pull from the GenePool.

--- src/main/java/vcreature/mainSimulation/GenePool.java
+++ src/main/java/vcreature/mainSimulation/GenePool.java
@@ -1,9 +1,29 @@
 package vcreature.mainSimulation;
 
+import vcreature.genotype.GenoFile;
+import vcreature.genotype.Genome;
+
+import java.util.LinkedList;
+
 /**
  * GenePool manages the list of creatures that exist.
  * Upon initialization it reads all genomes in GenePool folder
  */
 public class GenePool
 {
+  private static LinkedList<Genome> genomes;
+  static
+  {
+    genomes = GenoFile.loadGenePool();
+  }
+
+  public static Genome getRandom()
+  {
+    synchronized (genomes)
+    {
+      return genomes.getFirst();
+    }
+  }
+
+
 }
