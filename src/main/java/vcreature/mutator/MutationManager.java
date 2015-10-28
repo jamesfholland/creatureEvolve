f950a282fc782a29ecfa4712b4dbc901d2470299
Tyler Shelton <tshelt0n@unm.edu>
2015-10-27 20:46:57 -0600

    commit

--- src/main/java/vcreature/mutator/MutationManager.java
+++ src/main/java/vcreature/mutator/MutationManager.java
@@ -54,10 +54,10 @@ public class MutationManager
       parentGenome = testingGenome;
     }
 
-    boolean overRide =false;
+    boolean overRide =true;
     if(overRide)
     {
-
+      testingGenome = Adder.addBlock(parentGenome);
     }
     else
     {
