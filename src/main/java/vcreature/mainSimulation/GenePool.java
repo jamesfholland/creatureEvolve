4cfe2621140ef93ae63cae27a534875231503075
James Holland <jamesfholland@gmail.com>
2015-10-28 00:36:37 -0600

    Added empty GenePool class for managing the genepool

---
+++ src/main/java/vcreature/mainSimulation/GenePool.java
@@ -0,0 +1,9 @@
+package vcreature.mainSimulation;
+
+/**
+ * GenePool manages the list of creatures that exist.
+ * Upon initialization it reads all genomes in GenePool folder
+ */
+public class GenePool
+{
+}
