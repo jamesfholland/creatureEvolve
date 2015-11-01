b3af82cddd3d44e29b1a0067989a5bc731e9e71e
James Holland <jamesfholland@gmail.com>
2015-10-31 20:14:36 -0600

    Added Genetic Manager code and MergeType enum

--- src/main/java/vcreature/mutator/geneticMerger/GeneticManager.java
+++ src/main/java/vcreature/mutator/geneticMerger/GeneticManager.java
@@ -1,12 +1,26 @@
 package vcreature.mutator.geneticMerger;
 
-/**
- * Created by Dayloki on 10/28/2015.
- */
+import vcreature.genotype.Genome;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Manages genetic merger classes. Will choose which class to use in different situations
  */
 public class GeneticManager
 {
+  private LinkedList<GenomeTracker> testQueue;
+  GeneticManager()
+  {
+    testQueue = new LinkedList<>();
+  }
+
+
+class GenomeTracker
+{
+  private Genome parent1;
+  private Genome parent2;
+  private Genome genome;
+}
 }
--- src/main/java/vcreature/mutator/geneticMerger/MergeTypes/MergeType.java
+++ src/main/java/vcreature/mutator/geneticMerger/MergeTypes/MergeType.java
@@ -1,8 +1,46 @@
 package vcreature.mutator.geneticMerger.MergeTypes;
 
+import vcreature.genotype.Genome;
+
+import java.util.ArrayList;
+
 /**
  * Created by jholland on 10/31/15.
  */
 public enum MergeType
 {
+  CUTANDSPLICE()
+      {
+        @Override
+        public ArrayList<Genome> merge(Genome parent1, Genome parent2)
+        {
+          return CutAndSplice.cutAndSplice(parent1, parent2);
+        }
+      },
+  SINGLECROSSOVER()
+      {
+        @Override
+        public ArrayList<Genome> merge(Genome parent1, Genome parent2)
+        {
+          return SingleCrossover.singleCrossOver(parent1, parent2);
+        }
+      },
+  SWAPBLOCKSANDNEURONS()
+      {
+        @Override
+        public ArrayList<Genome> merge(Genome parent1, Genome parent2)
+        {
+          return null;
+        }
+      },
+  SWAPLEGS()
+      {
+        @Override
+        public ArrayList<Genome> merge(Genome parent1, Genome parent2)
+        {
+          return null;
+        }
+      };
+
+  public abstract ArrayList<Genome> merge(Genome parent1, Genome parent2);
 }
