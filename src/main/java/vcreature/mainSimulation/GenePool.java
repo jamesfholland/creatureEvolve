a59ca32da8b94f68bc7dac38cf239ba2955dc058
James Holland <jamesfholland@gmail.com>
2015-10-31 20:13:57 -0600

    Adding needed classes to GenePool

--- src/main/java/vcreature/mainSimulation/GenePool.java
+++ src/main/java/vcreature/mainSimulation/GenePool.java
@@ -12,20 +12,71 @@ import java.util.Random;
  */
 public class GenePool
 {
-  private static LinkedList<Genome> genomes;
+  private static final LinkedList<Genome> GENOMES;
   private static Random rand=new Random();
   static
   {
-    genomes = GenoFile.loadGenePool();
+    GENOMES = GenoFile.loadGenePool();
   }
 
+  /**
+   * Return a random Genome from the pool.
+   * @return a Genome
+   */
   public static Genome getRandom()
   {
-    synchronized (genomes)
+    synchronized (GENOMES)
     {
-      int index = Math.abs(rand.nextInt()) % genomes.size();
-      return genomes.get(index);
+      int index = Math.abs(rand.nextInt()) % GENOMES.size();
+      return GENOMES.get(index);
     }
+  }
+
+  /**
+   * Replace 2 parents with a child.
+   * @param newGenome Genome to store
+   * @param parent1 Genome to remove
+   * @param parent2 Genome to remove
+   */
+  public static void replace(Genome newGenome, Genome parent1, Genome parent2)
+  {
+    synchronized (GENOMES)
+    {
+      GENOMES.remove(parent1);
+      GENOMES.remove(parent2);
+      GENOMES.add(newGenome);
+    }
+  }
+
+  /**
+   * Replace a single parent with a child Genome
+   * @param newGenome Genome to store
+   * @param parent Genome to replace
+   */
+  public static void replace(Genome newGenome, Genome parent)
+  {
+    synchronized (GENOMES)
+    {
+      GENOMES.remove(parent);
+      GENOMES.add(newGenome);
+    }
+  }
+
+  /**
+   * Adds a Genome to the pool
+   * @param newGenome Genome to add
+   */
+  public static void add(Genome newGenome)
+  {
+    synchronized (GENOMES)
+    {
+      GENOMES.add(newGenome);
+    }
+  }
+  
+  public static Genome getBest()
+  {
+    return getRandom();
   }
 
 
