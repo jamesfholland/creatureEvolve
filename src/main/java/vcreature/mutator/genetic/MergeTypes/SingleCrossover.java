29e61bc4272b790f469a089563336f3c62e1179e
James Holland <jamesfholland@gmail.com>
2015-11-03 03:24:00 -0700

    Redid SingleCrossover logic to prevent problems with different sized genomes.

--- src/main/java/vcreature/mutator/genetic/MergeTypes/SingleCrossover.java
+++ src/main/java/vcreature/mutator/genetic/MergeTypes/SingleCrossover.java
@@ -25,28 +25,25 @@ public static ArrayList<Genome> singleCrossOver(Genome parent1,Genome parent2)
 
   ArrayList<Genome> children=new ArrayList<>();
 
-  Random rand=new Random();
-
   ArrayList<Gene> genes1=new ArrayList<>();
   ArrayList<Gene> genes2=new ArrayList<>();
 genes1= GeneticTools.getParentsGenes(parent1, parent2)[0];
 genes2=GeneticTools.getParentsGenes(parent1,parent2)[1];
-  int smallestGeneSize = genes1.size()>genes2.size() ? genes1.size() : genes2.size();
-  int crossoverPoint=rand.nextInt(smallestGeneSize+1)-1;
+  int smallestGeneSize = genes1.size() < genes2.size() ? genes1.size() : genes2.size();
+  int crossoverPoint=MainSim.RANDOM.nextInt(smallestGeneSize);
   Genome child1=new Genome(parent1.getRootSize(),parent1.getRootEulerAngles());
   //Child 1 will be made here
-  for (int i = 0; i < genes2.size(); i++)
+  for (int i = 0; i < crossoverPoint; i++)
   {
-    if(i<crossoverPoint)
-    {
       Gene gene=genes1.get(i);
       child1.addGeneBlock(gene.geneBlock);
       for (int j = 0; j <gene.geneNeurons.size() ; j++)
       {
         child1.addGeneNeuron(gene.geneNeurons.get(j));
       }
-    }else
-    {
+    }
+  for (int i = crossoverPoint; i < genes2.size(); i++)
+  {
       Gene gene=genes2.get(i);
       child1.addGeneBlock(gene.geneBlock);
       for (int j = 0; j <gene.geneNeurons.size() ; j++)
@@ -54,27 +51,26 @@ genes2=GeneticTools.getParentsGenes(parent1,parent2)[1];
         child1.addGeneNeuron(gene.geneNeurons.get(j));
       }
     }
-  }
+
 
   Genome child2=new Genome(parent2.getRootSize(),parent2.getRootEulerAngles());
-  for (int i = 0; i <genes1.size(); i++)
+  for (int i = 0; i < crossoverPoint; i++)
   {
-    if(i<crossoverPoint)
+    Gene gene=genes2.get(i);
+    child2.addGeneBlock(gene.geneBlock);
+    for (int j = 0; j <gene.geneNeurons.size() ; j++)
     {
-      Gene gene=genes2.get(i);
-      child2.addGeneBlock(gene.geneBlock);
-      for (int j = 0; j <gene.geneNeurons.size() ; j++)
-      {
-        child2.addGeneNeuron(gene.geneNeurons.get(j));
-      }
-    }else
+      child2.addGeneNeuron(gene.geneNeurons.get(j));
+    }
+
+  }
+  for (int i = crossoverPoint; i < genes1.size(); i++)
+  {
+    Gene gene=genes1.get(i);
+    child2.addGeneBlock(gene.geneBlock);
+    for (int j = 0; j <gene.geneNeurons.size() ; j++)
     {
-      Gene gene=genes1.get(i);
-      child2.addGeneBlock(gene.geneBlock);
-      for (int j = 0; j <gene.geneNeurons.size() ; j++)
-      {
-        child2.addGeneNeuron(gene.geneNeurons.get(j));
-      }
+      child2.addGeneNeuron(gene.geneNeurons.get(j));
     }
   }
   children.add(child1);
