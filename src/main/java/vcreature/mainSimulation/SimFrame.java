64c3c2600bbba58f89bbac3ce3fcd2016c2521cf
James Holland <jamesfholland@gmail.com>
2015-11-03 05:45:57 -0700

    Fixing unchecked cast exception.

--- src/main/java/vcreature/mainSimulation/SimFrame.java
+++ src/main/java/vcreature/mainSimulation/SimFrame.java
@@ -38,7 +38,7 @@ public class SimFrame extends JFrame implements ActionListener
   private JLabel threshold;
   private JTextField userThreshold = new JTextField();
   private String[] modes = {"Genetic/Hill Climbing", "Hill Climbing", "Genetic Algorithm"};
-  private JComboBox modeChange = new JComboBox(modes);
+  private JComboBox modeChange = new JComboBox<String>(modes);
   private JScrollPane creatureSelector = new JScrollPane();
   private Timer fitnessTracker;
   private DecimalFormat df = new DecimalFormat("#0.00");
