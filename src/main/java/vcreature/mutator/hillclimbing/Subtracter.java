ac008cafc676921bacb572fc2e0b2fc6fbf05e73
Tyler Shelton <tshelt0n@unm.edu>
2015-11-04 01:10:37 -0700

    added Javadoc for Subtractor

--- src/main/java/vcreature/mutator/hillclimbing/Subtracter.java
+++ src/main/java/vcreature/mutator/hillclimbing/Subtracter.java
@@ -13,6 +13,11 @@ import java.util.Random;
 public class Subtracter
 {
 
+  /**
+   * Takes a genome, then takes a block and subtracts it from the creature.
+   *
+   * @param genome a genome of a current creature that you want to change
+   * @return a new genome with a block that has been subtracted*/
   public static Genome subtractBlock(Genome genome)
   {
     Random rand=new Random();
@@ -42,31 +47,31 @@ public class Subtracter
   return newGenome;
 }
 
-  public static Genome subtractNeurons(Genome genome)
-  {
-    Random rand=new Random();
-    Genome newGenome=subtractNeurons(genome,rand.nextInt(genome.getGENE_BLOCKS().size()));
-    return newGenome;
-  }
-  public static Genome subtractNeurons(Genome genome,int index)
-  {
-    ArrayList<GeneBlock> geneBlocks=genome.getGENE_BLOCKS();
-    ArrayList<GeneNeuron> geneNeurons=genome.getGENE_NEURONS();
-   geneNeurons.remove(index);
-
-    Genome newGenome=new Genome(genome.getRootSize(),genome.getRootEulerAngles());
-    for (int i = 0; i <geneBlocks.size(); i++)
-    {
-
-        newGenome.addGeneBlock(geneBlocks.get(i));
-      if(i!=index)
-      {
-        for (int j = 0; j < geneNeurons.size(); j++)
-        {
-          if (geneNeurons.get(j).BLOCK_INDEX == i) newGenome.addGeneNeuron(geneNeurons.get(i));
-        }
-      }
-    }
-    return newGenome;
-  }
+//  public static Genome subtractNeurons(Genome genome)
+//  {
+//    Random rand=new Random();
+//    Genome newGenome=subtractNeurons(genome,rand.nextInt(genome.getGENE_BLOCKS().size()));
+//    return newGenome;
+//  }
+//  public static Genome subtractNeurons(Genome genome,int index)
+//  {
+//    ArrayList<GeneBlock> geneBlocks=genome.getGENE_BLOCKS();
+//    ArrayList<GeneNeuron> geneNeurons=genome.getGENE_NEURONS();
+//   geneNeurons.remove(index);
+//
+//    Genome newGenome=new Genome(genome.getRootSize(),genome.getRootEulerAngles());
+//    for (int i = 0; i <geneBlocks.size(); i++)
+//    {
+//
+//        newGenome.addGeneBlock(geneBlocks.get(i));
+//      if(i!=index)
+//      {
+//        for (int j = 0; j < geneNeurons.size(); j++)
+//        {
+//          if (geneNeurons.get(j).BLOCK_INDEX == i) newGenome.addGeneNeuron(geneNeurons.get(i));
+//        }
+//      }
+//    }
+//    return newGenome;
+//  }
 }
