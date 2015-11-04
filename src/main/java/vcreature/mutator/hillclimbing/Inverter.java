5822d832f092b6387f969a92142b53cf41bf97dc
Tyler Shelton <tshelt0n@unm.edu>
2015-11-04 00:09:02 -0700

    Adding Inverter Javadoc

--- src/main/java/vcreature/mutator/hillclimbing/Inverter.java
+++ src/main/java/vcreature/mutator/hillclimbing/Inverter.java
@@ -10,55 +10,89 @@ import vcreature.mutator.Cleaner;
 import java.util.ArrayList;
 
 /**
- * Inverts creatures and genes. Flips sign of neurons, flips entire genomes intelligently (turns creature over) or stupidly (ie flips arraylist)
+ * Inverts creatures and genes. Flips sign of neurons, flips entire genomes
+ * intelligently (turns creature over) or stupidly (ie flips arraylist)
  */
 public class Inverter
 {
-public static Genome basicInverter(Genome genome)
-{
-  Genome newGenome=new Genome(genome.getRootSize(),genome.getRootEulerAngles());
-  ArrayList<GeneBlock> geneBlocks=genome.getGENE_BLOCKS();
-  ArrayList<GeneNeuron> geneNeurons=genome.getGENE_NEURONS();
-
-  for (int i = 0; i < geneBlocks.size(); i++)
+  /**
+   * Takes a genome and will flip it upside down.
+   * First takes all the blocks and inverts the pivot points of them.
+   * Then it takes the Neurons and aflips there impulse signs.
+   * returns the "upside down creauture"
+   *
+   * @param genome a genome of a current creature that you want to change
+   * @return a new genome with everything being turned upside down.
+   */
+  public static Genome basicInverter(Genome genome)
   {
-    GeneBlock tempBlock=geneBlocks.get(i);
-    GeneBlock newBlock=new GeneBlock(tempBlock.PARENT_OFFSET,invertY(tempBlock.PARENT_PIVOT),invertY(tempBlock.PIVOT),tempBlock.SIZE,tempBlock.PARENT_HINGE_AXIS,tempBlock.HINGE_AXIS,tempBlock.EULER_ANGLES);
-    newGenome.addGeneBlock(newBlock);
-  }
-  for (int i = 0; i < geneNeurons.size(); i++)
-  {
-    GeneNeuron tempNeuron=geneNeurons.get(i);
-    GeneNeuron newNeuron=new GeneNeuron(tempNeuron.BLOCK_INDEX,tempNeuron.A_TYPE,tempNeuron.B_TYPE,tempNeuron.C_TYPE,tempNeuron.D_TYPE,tempNeuron.E_TYPE,tempNeuron.A_VALUE,tempNeuron.B_VALUE,tempNeuron.C_VALUE,-1*tempNeuron.D_VALUE,tempNeuron.E_VALUE,tempNeuron.BINARY_AB,tempNeuron.UNARY_AB,tempNeuron.BINARY_DE,tempNeuron.UNARY_DE);
-    newGenome.addGeneNeuron(newNeuron);
+    Genome newGenome =
+        new Genome(genome.getRootSize(), genome.getRootEulerAngles());
+    ArrayList<GeneBlock> geneBlocks = genome.getGENE_BLOCKS();
+    ArrayList<GeneNeuron> geneNeurons = genome.getGENE_NEURONS();
+
+    for (int i = 0; i < geneBlocks.size(); i++)
+    {
+      GeneBlock tempBlock = geneBlocks.get(i);
+      GeneBlock newBlock = new GeneBlock(tempBlock.PARENT_OFFSET,
+          invertY(tempBlock.PARENT_PIVOT), invertY(tempBlock.PIVOT),
+          tempBlock.SIZE, tempBlock.PARENT_HINGE_AXIS, tempBlock.HINGE_AXIS,
+          tempBlock.EULER_ANGLES);
+      newGenome.addGeneBlock(newBlock);
+    }
+    for (int i = 0; i < geneNeurons.size(); i++)
+    {
+      GeneNeuron tempNeuron = geneNeurons.get(i);
+      GeneNeuron newNeuron =
+          new GeneNeuron(tempNeuron.BLOCK_INDEX, tempNeuron.A_TYPE,
+              tempNeuron.B_TYPE, tempNeuron.C_TYPE, tempNeuron.D_TYPE,
+              tempNeuron.E_TYPE, tempNeuron.A_VALUE, tempNeuron.B_VALUE,
+              tempNeuron.C_VALUE, -1 * tempNeuron.D_VALUE, tempNeuron.E_VALUE,
+              tempNeuron.BINARY_AB, tempNeuron.UNARY_AB, tempNeuron.BINARY_DE,
+              tempNeuron.UNARY_DE);
+      newGenome.addGeneNeuron(newNeuron);
+    }
+
+    return newGenome;
   }
 
-  return newGenome;
-}
   private static ImmutableVector invertY(ImmutableVector immutableVector)
   {
-    return new ImmutableVector(immutableVector.X,-1*immutableVector.Y,immutableVector.Z);
+    return new ImmutableVector(immutableVector.X, -1 * immutableVector.Y,
+        immutableVector.Z);
   }
+
   public static Genome flipNeuron(Genome genome)
   {
-    genome=Cleaner.cleanGenome(genome);
-    Genome newGenome=new Genome(genome.getRootSize(),genome.getRootEulerAngles());
-    ArrayList<GeneBlock> geneBlocks=genome.getGENE_BLOCKS();
-    ArrayList<GeneNeuron> geneNeurons=genome.getGENE_NEURONS();
-    int index=MainSim.RANDOM.nextInt(geneNeurons.size());
-    for (int i = 0; i <geneBlocks.size() ; i++)
+    genome = Cleaner.cleanGenome(genome);
+    Genome newGenome =
+        new Genome(genome.getRootSize(), genome.getRootEulerAngles());
+    ArrayList<GeneBlock> geneBlocks = genome.getGENE_BLOCKS();
+    ArrayList<GeneNeuron> geneNeurons = genome.getGENE_NEURONS();
+    int index = MainSim.RANDOM.nextInt(geneNeurons.size());
+    for (int i = 0; i < geneBlocks.size(); i++)
     {
       newGenome.addGeneBlock(geneBlocks.get(i));
     }
-    for (int i = 0; i <geneNeurons.size() ; i++)
+    for (int i = 0; i < geneNeurons.size(); i++)
     {
-      if(i == index) newGenome.addGeneNeuron(flipNeuron(geneNeurons.get(index)));
-      else newGenome.addGeneNeuron(geneNeurons.get(i));
+      if (i == index)
+      {
+        newGenome.addGeneNeuron(flipNeuron(geneNeurons.get(index)));
+      }
+      else
+      {
+        newGenome.addGeneNeuron(geneNeurons.get(i));
+      }
     }
     return newGenome;
   }
+
   public static GeneNeuron flipNeuron(GeneNeuron neuron)
   {
-    return new GeneNeuron(neuron.BLOCK_INDEX,neuron.A_TYPE,neuron.B_TYPE,neuron.C_TYPE,neuron.D_TYPE,neuron.E_TYPE,neuron.A_VALUE,neuron.B_VALUE,neuron.C_VALUE,-1*neuron.D_VALUE,neuron.E_VALUE,neuron.BINARY_AB,neuron.UNARY_AB,neuron.BINARY_DE,neuron.UNARY_DE);
+    return new GeneNeuron(neuron.BLOCK_INDEX, neuron.A_TYPE, neuron.B_TYPE,
+        neuron.C_TYPE, neuron.D_TYPE, neuron.E_TYPE, neuron.A_VALUE,
+        neuron.B_VALUE, neuron.C_VALUE, -1 * neuron.D_VALUE, neuron.E_VALUE,
+        neuron.BINARY_AB, neuron.UNARY_AB, neuron.BINARY_DE, neuron.UNARY_DE);
   }
 }
