e23d8f8ba03e2e63f25b3b51e45896ec46e2e53e
James Holland <jamesfholland@gmail.com>
2015-11-04 02:46:24 -0700

    Changed GenePool size to 1000 for testing on lab computers

--- src/main/java/vcreature/mainSimulation/GenePool.java
+++ src/main/java/vcreature/mainSimulation/GenePool.java
@@ -18,7 +18,7 @@ public class GenePool
    * Get a copy or add a synchronized method to do what you need.
    */
   private static final LinkedList<Genome> GENOMES;
-  private static final int MINIMUM_POOL_SIZE = 100;
+  private static final int MINIMUM_POOL_SIZE = 1000;
   static
   {
     GENOMES = GenoFile.loadGenePool();
